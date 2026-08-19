c381d5aee80d93320190ca18cbf306c98bac7a44
robinshiji <robinshiji3@gmail.com>
2026-08-19 11:09:16 +0530

    date format changed

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -3,7 +3,7 @@ export const formatDate = (dateString: string | null | undefined): string => {
   const parts = dateString.split('-');
   if (parts.length === 3) {
     const [year, month, day] = parts;
-    return `${day} ${month} ${year.slice(-2)}`;
+    return `${day}-${month}-${year.slice(-2)}`;
   }
   return dateString;
 };
